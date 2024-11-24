ad9b45483f0b5eae568f143eec3579390cd5dc4a
Mateus Araujo Santos <mateuaraujo01@gmail.com>
2024-11-24 11:42:26 -0300

    criando media de Tarefas dentro do prazo em colaborador

--- src/main/java/com/feedbackFusion/dto/IndicadoresDTO.java
+++ src/main/java/com/feedbackFusion/dto/IndicadoresDTO.java
@@ -5,10 +5,22 @@ import java.util.List;
 import com.feedbackFusion.service.*;
 
 public class IndicadoresDTO {
-    public FeedbackService feedbackService = new FeedbackService();
-    public EquipeService equipeService = new EquipeService();
+
+    public FeedbackService feedbackService;
+    public EquipeService equipeService;
+    public TarefaService tarefaService;
 
     public double mediaFeedbacks(){ 
          return (double) (feedbackService.getAll().size() / equipeService.getAllColaboradores().size());
+    }
+    public double mediaTarefasNoPrazoColaborador(){
+        List<TarefaDTO> tarefas = tarefaService.getAll();
+        List<TarefaDTO> tarefasNoPrazo = new ArrayList<>();
+        for(TarefaDTO tarefa : tarefas){
+            if(tarefa.getDataConclusao().isBefore(tarefa.getDataPrazo())){
+                tarefasNoPrazo.add(tarefa);
+            }
+        }
+        return (double) (tarefasNoPrazo.size() / equipeService.getAllColaboradores().size());
     }
 }
